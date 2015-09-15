50fad2541cb9f8e651b7550fb9fa7eebf05069da
Sydothekido11 <sydnemelka@gmail.com>
2015-09-15 13:07:40 -0600

    added get and set methods

--- src/marshmallows/model/MarshmallowMonster.java
+++ src/marshmallows/model/MarshmallowMonster.java
@@ -25,6 +25,70 @@ public class MarshmallowMonster
 			
 		}
 		
+		public String getMonsterName()
+		{
+			return monsterName;
+		}
+
+		public int getMonsterEyes()
+		{
+			return monsterEyes;
+		}
+		
+		public int getMonsterNoses()
+		{
+			return monsterNoses;
+		}
+		
+		public double getMonsterLegs()
+		{
+			return monsterLegs;
+		}
+		
+		public double getMonsterHair()
+		{
+			return monsterHair;
+		}
+		
+		public boolean getMonsterBellyButton()
+		{
+			return monsterBellyButton;
+		}
+		
+		
+		
+		public void setMonsterName(String monsterName)
+		{
+			this.monsterName = monsterName;
+		}
+		
+		public void setMonsterEyes(int monsterEyes)
+		{
+			this.monsterEyes = monsterEyes;
+		}
+		
+		public void setMonsterNoses(int monsterNoses)
+		{
+			this.monsterNoses = monsterNoses;
+		}
+		
+		public void setMonsterLegs(int monsterLegs)
+		{
+			this.monsterLegs = monsterLegs;
+		}
+		
+		public void setMonsterHair(int monsterHair)
+		{
+			this.monsterHair = monsterHair;
+		}
+		
+		public void setMonsterBellyButton(boolean monsterBellyButton)
+		{
+			this.monsterBellyButton = monsterBellyButton;
+		}
+		
+		
+		
 		public String toString()
 		{
 			String monsterInfo = "My monster has " + monsterLegs + " legs and its name is " + monsterName
