f64b6aa9c78a201f460ebfad27da7e7b1f0282cf
Sydothekido11 <sydnemelka@gmail.com>
2015-09-15 13:29:48 -0600

    added questions method

--- src/marshmallows/controller/MarshmallowController.java
+++ src/marshmallows/controller/MarshmallowController.java
@@ -2,11 +2,14 @@ package marshmallows.controller;
 
 import marshmallows.model.MarshmallowMonster;
 import marshmallows.view.MonsterDisplay;
+import java.util.Scanner;
+
 
 public class MarshmallowController
 {
 	private MarshmallowMonster sydoMonster;
 	private MonsterDisplay myDisplay;
+	private Scanner monsterScanner;
 	
 	public MarshmallowController()
 	{
@@ -17,12 +20,23 @@ public class MarshmallowController
 		double hair = 0.5;
 		boolean hasBellyButton = false;
 		
+		monsterScanner = new Scanner(System.in);
+		
 		myDisplay = new MonsterDisplay();
 		sydoMonster = new MarshmallowMonster(name, eyes, hasBellyButton, noses, legs, hair );
 	}
-		public void start()
+		
+	public void start()
 		{
 			myDisplay.displayInfo(sydoMonster.toString());
+			askQuestions();
+			myDisplay.displayInfo("Updated monster info: " + sydoMonster.toString());
 		}
 	
+		private void askQuestions()
+		{
+			System.out.println("I want a new name for a monster, type one please! (p_p)");
+			String newMonsterName = monsterScanner.next();
+			sydoMonster.setMonsterName(newMonsterName);
+		}
 }
