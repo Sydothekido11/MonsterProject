2fbb26dce07ef0ffaf0f7ccaa0269513f1f96dd4
Sydothekido11 <sydnemelka@gmail.com>
2015-09-30 13:50:40 -0600

    Began modifying monster project.

--- src/marshmallows/model/MarshmallowMonster.java
+++ src/marshmallows/model/MarshmallowMonster.java
@@ -9,9 +9,9 @@ public class MarshmallowMonster
 	private double monsterHair;
 	private boolean monsterBellyButton;
 	
-		private MarshmallowMonster()
+		public MarshmallowMonster()
 		{
-			
+			 
 		}
 		
 		public MarshmallowMonster(String monsterName, int monsterEyes, boolean monsterBellyButton, int monsterNoses, double monsterLegs, double monsterHair)
@@ -89,8 +89,7 @@ public class MarshmallowMonster
 		
 		public String toString()
 		{
-			String monsterInfo = "My monster has " + monsterLegs + " legs and its name is " + monsterName
-									+ " with " + monsterEyes + " eyes and " + monsterNoses + " noses and amazing " + monsterHair + " hair and lastly " + monsterBellyButton + " belly buttons.";
+			String monsterInfo = monsterName + " has " + monsterEyes + " and " + monsterNoses + " noses and " + monsterLegs + " legs and " + monsterHair " hair " + monsterBellyButton + "belly button.";
 			
 			return monsterInfo;
 		}
